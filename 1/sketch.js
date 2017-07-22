9dbd098c3887caaf02826c6c18f508c327b01b42
EveChuang <echuang98@gmail.com>
2017-07-22 21:28:24 +0800

    first commit

---
+++ 1/sketch.js
@@ -0,0 +1,13 @@
+// this is comment
+// type whatever you want here
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(100,200,100,1);
+  stroke(100,100,200,10);
+  ellipse(200, 100, 50, 50);
+
+}
